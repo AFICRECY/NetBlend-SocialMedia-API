eb6180876944f0f65f5bf8fbf4842b5ac296fa78
Afi Nkhume-Crecy <Aficrecy@gmail.com>
2023-05-21 00:00:51 -0700

    created the thoughtRoute and the userRoute to handle the various different crud methods fir thoughts

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const {
+  getUser,
+  getSingleUser,
+  createUser,
+  deleteUser,
+  addReaction,
+  removeReaction,
+} = require('../../controllers/userController');
+
+// /api/students
+router.route('/').get(getUser).post(createUser);
+
+// /api/students/:studentId
+router.route('/:userId').get(getSingleUser).delete(deleteUser);
+
+// /api/students/:studentId/assignments
+router.route('/:userId/reaction').post(addReaction);
+
+// /api/students/:studentId/assignments/:assignmentId
+router.route('/:userId/reaction/:reactionId').delete(removeReaction);
+
+module.exports = router;
